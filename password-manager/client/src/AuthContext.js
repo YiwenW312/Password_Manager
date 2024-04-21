be753a7223a78ac4c55ed6327d4bb248dad2dca7
Yiwen Wang <124082584+YiwenW312@users.noreply.github.com>
2024-04-20 19:43:28 -0700

    frontend API fix

--- password-manager/client/src/AuthContext.js
+++ password-manager/client/src/AuthContext.js
@@ -11,7 +11,7 @@ export function AuthProvider({ children }) {
 
   const login = async (username, password) => {
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('http://localhost:3000/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
