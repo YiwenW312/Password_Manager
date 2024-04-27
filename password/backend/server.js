20683758d54ae2eba5f2d2cdd169b8680021fe84
Yiwen Wang <124082584+YiwenW312@users.noreply.github.com>
2024-04-27 03:25:01 -0700

    try

--- password/backend/server.js
+++ password/backend/server.js
@@ -13,7 +13,9 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*' 
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
