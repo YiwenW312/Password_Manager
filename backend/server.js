351d8aef30609f39253178a77f39bae0a26f37e6
Yiwen Wang <124082584+YiwenW312@users.noreply.github.com>
2024-04-28 19:32:25 -0700

    ignore favicon

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '.', 'frontend', 'build','public', 'index.html'));
 });
 
+app.get('/favicon.ico', (req, res) => res.status(204));
+
 // Handle 404 - Resource not found
 app.use((req, res, next) => {
   res.status(404).send({ message: 'Route not found' });
